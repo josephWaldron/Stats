2b2b6b9c5995e4715c1cd7c82949f353e9e3d92f
josephwaldron <waldrojo@kean.edu>
2022-01-23 15:57:21 -0500

    added paths to python

--- python/test.py
+++ python/test.py
@@ -4,4 +4,5 @@ print(x)
 # open power shell and type py -3 -m venv .venv
 # open powershell and type 'py -m pip install -U radian'
 #py -m radian\
-# radian path C:\Users\Joseph Waldron\AppData\Local\Programs\Python\Python310\Scripts\radian.exe
+# radian path C:\Users\Joseph Waldron\AppData\Local\Programs\Python\Python310\Scripts\radian.exe on desk
+#C:\Users\josep\AppData\Local\Programs\Python\Python310\Scripts\radian.exe on laptop
