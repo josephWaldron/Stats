ee5e67f5535a826ceddcec9693cc501f1ddb8013
JosephWaldron <waldrojo@kean.edu>
2022-01-24 14:41:13 -0500

    halphway

---
+++ stats/homework1.r
@@ -0,0 +1,40 @@
+137 + 242
+
+1206 - 373
+
+547 * 23
+
+8840 / 17
+
+11^3
+
+sqrt(64)
+
+8000^(1/3)
+
+E1_1 <- c(81, 17, 7, 55, 2, 98, 71, 47, 19, 8, 3, 10, 28, 65, 80)
+
+E1_1
+
+E1_1 <- sort(E1_1)
+
+E1_1
+median(E1_1)
+min(E1_1)
+max(E1_1)
+sum(E1_1)
+mean(E1_1)
+length(E1_1)
+
+mean <- sum(E1_1) / length(E1_1)
+mean
+
+head(LakeHuron, 3)
+length(LakeHuron)
+min(LakeHuron)
+max(LakeHuron)
+mean(LakeHuron)
+median(LakeHuron)
+
+hist(LakeHuron, col = 'blue')
+
