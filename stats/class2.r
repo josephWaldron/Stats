22957c7132ae1542345779051a938e73594e0db3
JosephWaldron <waldrojo@kean.edu>
2022-01-25 11:40:21 -0500

    histogram

--- stats/class2.r
+++ stats/class2.r
@@ -3,3 +3,4 @@ crash <- read.csv("Data Sets/CRASH.csv") #assigning file to crash variable
 head(crash, 5)#prints first 5 rows
 table(crash$CLASS)#create table of the CLASS colum
 barplot(table(crash$CLASS))#created barplot of CLASS
+hist(crash$WEIGHT)
