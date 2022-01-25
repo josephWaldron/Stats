731c0130c78b6da2795713c355f52ab711b1a0d3
JosephWaldron <waldrojo@kean.edu>
2022-01-25 11:38:47 -0500

    barplot

--- stats/class2.r
+++ stats/class2.r
@@ -2,3 +2,4 @@ setwd("~/")  #default is documents folder on pc
 crash <- read.csv("Data Sets/CRASH.csv") #assigning file to crash variable
 head(crash, 5)#prints first 5 rows
 table(crash$CLASS)#create table of the CLASS colum
+barplot(table(crash$CLASS))#created barplot of CLASS
