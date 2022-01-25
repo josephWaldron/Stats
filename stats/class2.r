e58d75aa9e280676f379addb148df1b3f50393f0
JosephWaldron <waldrojo@kean.edu>
2022-01-25 11:46:55 -0500

    plot added

--- stats/class2.r
+++ stats/class2.r
@@ -4,3 +4,4 @@ head(crash, 5)#prints first 5 rows
 table(crash$CLASS)#create table of the CLASS colum
 barplot(table(crash$CLASS))#created barplot of CLASS NON NUMERIC
 hist(crash$WEIGHT)#creates histogram with the WEIGHT column NUMERIC
+plot(crash$DRIVSTAR, crash$PASSSTAR)#shows relationship between 2 numerical variables
