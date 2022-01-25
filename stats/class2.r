2847013b9cdd3db18aa1e559d7e02267114ad49d
JosephWaldron <waldrojo@kean.edu>
2022-01-25 11:41:14 -0500

    qualitative and quantitative data

--- stats/class2.r
+++ stats/class2.r
@@ -2,5 +2,5 @@ setwd("~/")  #default is documents folder on pc
 crash <- read.csv("Data Sets/CRASH.csv") #assigning file to crash variable
 head(crash, 5)#prints first 5 rows
 table(crash$CLASS)#create table of the CLASS colum
-barplot(table(crash$CLASS))#created barplot of CLASS
-hist(crash$WEIGHT)
+barplot(table(crash$CLASS))#created barplot of CLASS NON NUMERIC
+hist(crash$WEIGHT)#creates histogram with the WEIGHT column NUMERIC
