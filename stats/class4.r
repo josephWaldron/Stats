63f106b4840b672ef7a0c9906f60c57f10e78d7d
josephwaldron <waldrojo@kean.edu>
2022-02-01 11:54:32 -0500

    1

--- stats/class4.r
+++ stats/class4.r
@@ -1,2 +1,9 @@
+install.packages("readxl")
+library(readxl)
 setwd("~/")  #default is documents folder on pc
 crash <- read.csv("Data Sets/CRASH.csv") #assigning file to crash variable
+head(crash, 4)
+crash$WEIGHT
+mean(crash$WEIGHT)
+sd(crash$WEIGHT)
+boxplot(crash$WEIGHT)
