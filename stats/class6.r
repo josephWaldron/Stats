9be9992842684da67ab3e793c2f6ef809e307f2f
josephwaldron <waldrojo@kean.edu>
2022-02-17 12:07:56 -0500

    init

--- stats/class6.r
+++ stats/class6.r
@@ -3,4 +3,5 @@ i <- choose(52, 5) #combinaitons
 x <- choose(4, 3) * choose(4, 2)
 x / i
 pbinom(45, 50, .9) - pbinom(44, 50, .9)      #binomial prob (x, n, p) this gives answer for x = 45, n=50 p=.9
-pbinom(40, 50, .9) - pbinom(39, 50, .9)
+pbinom(39, 50, .9) - pbinom(38, 50, .9)
+pbinom(50, 50, .9) - pbinom(39, 50, .9)
