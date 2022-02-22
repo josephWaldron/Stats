39e8e613053caf7485fe7492c9c39206fdb065b7
josephwaldron <waldrojo@kean.edu>
2022-02-22 12:14:11 -0500

    added ppois

--- stats/class6.r
+++ stats/class6.r
@@ -5,3 +5,11 @@ x / i
 pbinom(45, 50, .9) - pbinom(44, 50, .9)      #binomial prob (x, n, p) this gives answer for x = 45, n=50 p=.9
 pbinom(39, 50, .9) - pbinom(38, 50, .9)
 pbinom(50, 50, .9) - pbinom(39, 50, .9)
+
+pbinom(9, 20, .33)  #cumulative from 0->9
+pbinom(7, 20, .33) - pbinom(6, 20, .33)  #for x = 7
+1 - pbinom(5, 20, .33)  #for x > 5 = 1 - p(x <= 5)
+
+1 - pbinom(10, 20, .25)
+
+ppois(5, 5) - ppois(4, 5)  #poison p(3) where mean is 5
